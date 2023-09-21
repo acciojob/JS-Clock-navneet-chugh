cd82d9bd0161f7cf3ee0e0fb60660e374db31e3c
navneet-chugh <navneetchugh15@gmail.com>
2023-09-21 20:24:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+function setClock() {
+  const hourHand = document.querySelector('.hour-hand');
+  const minHand = document.querySelector('.min-hand');
+  const secondHand = document.querySelector('.second-hand');
+
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondDegrees = ((seconds / 60) * 360) + 90;
+  const minuteDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+  const hourDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  minHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setClock, 1000); // Update every second
+
+setClock(); // Initial call to set the initial position
